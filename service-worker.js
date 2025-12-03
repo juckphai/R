e3535e8c38429d8f9acb750aaec41571ed13e8b3
juckphai <llsvuslnw@gmail.com>
2025-12-03 09:53:43 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const staticCacheName = 'activity-tracker-static-v298';
-const dynamicCacheName = 'activity-tracker-dynamic-v298';
+const staticCacheName = 'activity-tracker-static-v798';
+const dynamicCacheName = 'activity-tracker-dynamic-v798';
 
 // ไฟล์ที่ต้องการ cache
 const assets = [
